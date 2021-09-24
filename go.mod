da2b7d6b435d31ca95bc36e0cdf27177cf6ec8ec
hgfgdsy <1031010120@qq.com>
2021-09-24 22:14:22 +0800

    rrr

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/hgfgdsy/shorttest1
 
 go 1.15
-
-require github.com/hgfgdsy/shorttest2 v0.0.3
--- t1.go
+++ t1.go
@@ -2,10 +2,8 @@ package shorttest1
 
 import(
 	"fmt"
-	"github.com/hgfgdsy/shorttest2"
 )
 
 func test1(){
 	fmt.Println("123")
-	shorttest2.test2()
 }
