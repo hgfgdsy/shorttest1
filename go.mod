dc4f48c7a07b97daba256901cc9a217c40173a97
hgfgdsy <1031010120@qq.com>
2021-09-24 22:17:54 +0800

    rere

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/hgfgdsy/shorttest1
 
 go 1.15
+
+require github.com/hgfgdsy/shorttest2 v0.0.4
--- t1.go
+++ t1.go
@@ -2,8 +2,10 @@ package shorttest1
 
 import(
 	"fmt"
+	"github.com/hgfgdsy/shorttest2"
 )
 
 func test1(){
 	fmt.Println("123")
+	shorttest2.test2()
 }
